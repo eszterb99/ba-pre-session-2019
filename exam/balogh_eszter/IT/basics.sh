fb429ce9b53d791945fdecbbfd6b816c87f39106
Eszter99 <eszter.balogh99@gmail.com>
2019-09-14 19:26:05 +0200

    modified basics.sh

--- exam/balogh_eszter/IT/basics.sh
+++ exam/balogh_eszter/IT/basics.sh
@@ -1,4 +1,4 @@
-mkdir balogh_eszter/IT/made_by_my_script
-cd balogh_eszter
+mkdir ba-pre-session-2019/exam/balogh_eszter/IT/made_by_my_script
+cd ba-pre-session-2019/exam/balogh_eszter
 ls > IT/made_by_my_script/list_of_contents.txt
 
